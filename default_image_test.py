3553b38f8c25701dbb832667d2b21ead75653a8e
Jacob <jpavelka@umich.edu>
2024-08-05 17:48:03 -0400

    created image test

--- default_image_test.py
+++ default_image_test.py
@@ -6,13 +6,13 @@ path = os.environ.get("BLENDER_PATH")
 class B_Connect():
     def __init__(self,path):
         self.path = path
-        bpy.app.binary_path = path
+        # bpy.app.binary_path = path
         # connect to blender
 
 
 if __name__=="__main__":
     path = os.environ.get("BLENDER_PATH")
     con = B_Connect(path)
-    bpy.context.scene.render.filepath = "test"
-    bpy.context.scene.render.image_settings.file_format = "png"
-    bpy.ops.render.render()
+    bpy.context.scene.render.filepath = r"C:\Users\Jacob\projects\solids_vis\test.png"
+    bpy.context.scene.render.image_settings.file_format = "jpeg"
+    bpy.ops.render.render(write_still=True)
