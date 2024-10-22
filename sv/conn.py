710cdc85a52952355202d5ea07aa042559f66d2c
Sayut <jsayut@med.umich.edu>
2024-10-22 18:22:41 -0400

    added different object types and object kwargs to animated object, issues 16 and 17

--- sv/conn.py
+++ sv/conn.py
@@ -9,7 +9,15 @@ class Conn():
         self.bpy=bpy
         self.primitive_objs = {
             "cube":self.bpy.ops.mesh.primitive_cube_add,
-            "uv_sphere":self.bpy.ops.mesh.primitive_uv_sphere_add
+            "uv_sphere":self.bpy.ops.mesh.primitive_uv_sphere_add,
+            "torus":self.bpy.ops.mesh.primitive_torus_add,
+            "monkey":self.bpy.ops.mesh.primitive_monkey_add,
+            "circle":self.bpy.ops.mesh.primitive_circle_add,
+            "plane":self.bpy.ops.mesh.primitive_plane_add,
+            "cone":self.bpy.ops.mesh.primitive_cone_add,
+            "cylinder":self.bpy.ops.mesh.primitive_cylinder_add,
+            "grid":self.bpy.ops.mesh.primitive_grid_add,
+            "ico_sphere":self.bpy.ops.mesh.primitive_ico_sphere_add
         }
-    def get_primitive(self,obj_type):
-        return self.primitive_objs[obj_type]()
+    def get_primitive(self,obj_type,**kwargs):
+        return self.primitive_objs[obj_type](**kwargs)
