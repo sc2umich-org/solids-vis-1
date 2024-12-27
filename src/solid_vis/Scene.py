233e06b97d1e0b5e45c4b8a680a27880975d8f09
Jacob <jpavelka@umich.edu>
2024-12-27 12:59:39 -0500

    got animations to save, but cannot get camera to move

--- src/solid_vis/Scene.py
+++ src/solid_vis/Scene.py
@@ -1,4 +1,5 @@
 import solid_vis.conn as conn
+from solid_vis.AnimatedObject import AnimatedObject
 import os
 
 class Scene():
@@ -11,6 +12,10 @@ class Scene():
                 self.remove_cube()
             except KeyError:
                 pass
+
+        # set num frames to 1
+        self.conn.bpy.context.scene.frame_end=1
+        self.conn.bpy.context.scene.frame_start = 0
     
     def remove_cube(self):
         # select cube
@@ -34,10 +39,30 @@ class Scene():
         else:
             print("format must be JPEG or PNG")
 
+    def render_anim(self,anim_name,local_render_path):
+        bpy = self.conn.bpy
+        file_path = os.getcwd()
+        out_path = file_path + f"/{local_render_path}/renders/{anim_name}"
+        bpy.context.scene.render.filepath = out_path
+        bpy.context.scene.render.threads=4
+        bpy.context.scene.render.image_settings.file_format = "FFMPEG"
+        bpy.ops.render.render(animation=True,write_still=True)
+
     def get_camera(self):
         bpy = self.conn.bpy
 
         return bpy.data.objects["Camera"]
     
-    def animate_camera(self,pos,orientation):
-        
+    def animate_camera(self,pos,orientation,frames):
+        bpy = self.conn.bpy
+        camera_obj = self.get_camera()
+        for p_i,o_i,f_i in zip(pos,orientation,frames):
+            print(camera_obj.rotation_euler)
+            print(camera_obj.location)
+            camera_obj.location = p_i
+            camera_obj.keyframe_insert("location",frame = f_i)
+            o_i = self.conn.mathutils.Euler(o_i)
+            camera_obj.rotation_euler = o_i
+            camera_obj.keyframe_insert("rotation_euler", frame=f_i)
+        bpy.context.scene.camera = camera_obj
+        bpy.context.scene.camera.data.angle = 90*(3.14/180.0)
--- tests/moving_sphere.py
+++ tests/moving_sphere.py
@@ -1,20 +1,22 @@
 import numpy as np
 import sys
+from solid_vis.conn import Conn
+from solid_vis.Scene import Scene
+from solid_vis.AnimatedObject import AnimatedObject
 sys.path.append('.')
-import sv
 
 
 
 if __name__=="__main__":
 
     motion_data = np.array([[x,0,0] for x in np.arange(-10,10,1)])
-    bpy_conn = sv.conn.Conn()
-    scene = sv.Scene.Scene(bpy_conn)
-    obj = sv.AnimatedObject.AnimatedObject(
+    bpy_conn = Conn()
+    scene = Scene(bpy_conn)
+    obj = AnimatedObject(
         bpy_conn,
         motion_data,
         "uv_sphere",
         "test_sphere",
     )
 
-    scene.render("moving sphere","tests", "JPEG")
+    scene.render_anim("moving sphere","tests")
