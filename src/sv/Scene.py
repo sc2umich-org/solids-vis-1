fe8e92bee863497d6717e85dbb302db9a584d792
Jacob <jpavelka@umich.edu>
2024-12-27 10:23:16 -0500

    animation changes

--- src/sv/Scene.py
+++ src/sv/Scene.py
@@ -11,7 +11,7 @@ class Scene():
                 self.remove_cube()
             except KeyError:
                 pass
-
+    
     def remove_cube(self):
         # select cube
         cube = self.conn.bpy.data.objects["Cube"]
@@ -33,3 +33,11 @@ class Scene():
 
         else:
             print("format must be JPEG or PNG")
+
+    def get_camera(self):
+        bpy = self.conn.bpy
+
+        return bpy.data.objects["Camera"]
+    
+    def animate_camera(self,pos,orientation):
+        
